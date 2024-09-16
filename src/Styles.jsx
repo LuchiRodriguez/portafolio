582e766778eef9d7e65b4ed8071c4b1067906a5c
Luchi <luli79302@gmail.com>
2024-09-16 13:32:00 +0200

    .

--- src/Styles.jsx
+++ src/Styles.jsx
@@ -172,7 +172,8 @@ export const StyledContact = styled.div`
   section {
     display: inline-block;
     width: 470px;
-    height: 150px;
+    height: 200px;
+    padding-top: 30px;
     margin-left: 50px;
   }
   h3 {
